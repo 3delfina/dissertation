f6829907d4e2a26ff4ed1193ccc07c9226fe8311
3delfina <3delfina@gmail.com>
2020-10-24 17:24:13 +0100

    Moved all django files up by one dir to work on heroku.

---
+++ obfuscator/forms.py
@@ -0,0 +1,9 @@
+# forms.py
+from django import forms
+from .models import *
+
+class ParticipantForm(forms.ModelForm):
+
+    class Meta:
+        model = Participant
+        fields = ['participant_id', 'participant_photo']
