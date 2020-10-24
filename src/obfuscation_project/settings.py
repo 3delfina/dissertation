f71191b0cc0044f7036224df016fd092e4a6aa9b
Marija Mumm <3delfina@gmail.com>
2020-10-24 16:59:51 +0100

    Configured django app for heroku.

--- src/obfuscation_project/settings.py
+++ src/obfuscation_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +128,6 @@ STATIC_URL = "/static/"
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
