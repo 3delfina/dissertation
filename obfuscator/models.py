b57673f3cd5d46d41bee57cb879393b9d948e1fc
3delfina <3delfina@gmail.com>
2020-11-04 22:08:30 +0000

    Fixed file upload, the image is displayed.

--- obfuscator/models.py
+++ obfuscator/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# models.py
+
+class Participant(models.Model):
+    participant_id = models.CharField(max_length=50, unique=True)
+    participant_photo = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.participant_id
--- obfuscator/urls.py
+++ obfuscator/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('', views.index, name='index'),  
+    path('process', views.process, name='process')
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
