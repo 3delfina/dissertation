322e855e44938df201c177f1c3d6c277082cf5f9
3delfina <3delfina@gmail.com>
2020-12-02 13:31:56 +0000

    Replicated blur and pixelate values from experiments in papers.

--- obfuscator/obfuscate.py
+++ obfuscator/obfuscate.py
@@ -5,6 +5,7 @@ import cv2
 import imutils
 import os
 import random
+from PIL import Image, ImageFilter, ImageDraw
 
 
 def blur_face(image, factor=3.0):
@@ -16,18 +17,28 @@ def blur_face(image, factor=3.0):
     # ensure the width and height of the kernel are odd
     kW = kW - 1 if kW % 2 == 0 else kW
     kH = kH - 1 if kH % 2 == 0 else kH
-    return cv2.GaussianBlur(image, (kW, kH), sigmaX=0)
+    blurred_image = image.filter(ImageFilter.GaussianBlur(radius=4))
+    return blurred_image # cv2.GaussianBlur(image, (kW, kH), sigmaX=0)
 
 
 def blur_image(img_path, img_path_final, faces):
-    img = cv2.imread(img_path)
-    # img = imutils.resize(img, width=500)
-    count = 1
+    # img = cv2.imread(img_path)
+    # # img = imutils.resize(img, width=500)
+    # count = 1
+    # for (x, y, w, h) in faces:
+    #     region_of_interest = img[y:y + h, x:x + w]
+    #     img[y:y + h, x:x + w] = blur_face(region_of_interest)
+    #     count += 1
+    # cv2.imwrite(img_path_final, img)
+
+    image = Image.open(img_path)
     for (x, y, w, h) in faces:
-        region_of_interest = img[y:y + h, x:x + w]
-        img[y:y + h, x:x + w] = blur_face(region_of_interest)
-        count += 1
-    cv2.imwrite(img_path_final, img)
+        box = (x, y, x+w, y+h)
+        ic = image.crop(box)
+        ic = ic.filter(ImageFilter.GaussianBlur(radius=4))
+        image.paste(ic, box)
+    image.save(img_path_final)
+    # image.show()
 
 
 def pixelate_face(image):
@@ -39,14 +50,25 @@ def pixelate_face(image):
 
 
 def pixelate_image(img_path, img_path_final, faces):
-    print("from pixealate")
-    print(img_path)
-    print(img_path_final)
-    img = cv2.imread(img_path)
+    # Replicate Pixelate - Mosaic in Photoshop, cell size 15 square.
+    # print("from pixealate")
+    # print(img_path)
+    # print(img_path_final)
+    # img = cv2.imread(img_path)
+    # for (x, y, w, h) in faces:
+    #     region_of_interest = img[y:y + h, x:x + w]
+    #     img[y:y + h, x:x + w] = pixelate_face(region_of_interest)
+    # cv2.imwrite(img_path_final, img)
+
+    image = Image.open(img_path)
     for (x, y, w, h) in faces:
-        region_of_interest = img[y:y + h, x:x + w]
-        img[y:y + h, x:x + w] = pixelate_face(region_of_interest)
-    cv2.imwrite(img_path_final, img)
+        box = (x, y, x+w, y+h)
+        face = image.crop(box)
+        imgSmall = face.resize((15,15),resample=Image.BILINEAR)
+        # Scale back up using NEAREST to original size
+        result = imgSmall.resize(face.size,Image.NEAREST)
+        image.paste(result, box)
+    image.save(img_path_final)
 
 
 def _get_faces(img):
