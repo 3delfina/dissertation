cf9951edce4d4182e072f9631455d7a9905de27d
3delfina <3delfina@gmail.com>
2020-12-22 18:20:45 +0000

    Added user id validation, negative values are not allowed.

--- obfuscator/models.py
+++ obfuscator/models.py
@@ -1,12 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 import time
 
 # Create your models here.
 # models.py
 
+def is_positive(value):
+    if not value.isdigit():
+        raise ValidationError(
+            _('%(value)s is not a non-negative integer.'),
+            params={'value': value},
+        )
 
 class Participant(models.Model):
-    participant_id = models.CharField(max_length=50, unique=True)
+    participant_id = models.CharField(max_length=50, unique=True, validators=[is_positive])
     participant_photo = models.ImageField(upload_to='images/')
     faces_location_arr = models.CharField(max_length=200, blank=True)
     face_count = models.IntegerField(default=0, blank=True)
