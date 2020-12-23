71851793f4c36a03ea8724d8708893193f12d474
3delfina <3delfina@gmail.com>
2020-12-23 01:30:32 +0000

    Removed unused fn in DeepPrivacy, shortened requirements.

--- DeepPrivacy/deep_privacy/inference/anonymizer.py
+++ DeepPrivacy/deep_privacy/inference/anonymizer.py
@@ -1,6 +1,6 @@
 import os
 os.environ["IMAGEIO_FFMPEG_EXE"] = "/usr/bin/ffmpeg"
-import moviepy.editor as mp
+# import moviepy.editor as mp
 import cv2
 import numpy as np
 import pathlib
@@ -83,35 +83,35 @@ class Anonymizer:
             # debug_impath = new_path.parent.joinpath(new_name)
             # cv2.imwrite(str(debug_impath), to_save[:, :, ::-1])
 
-    def anonymize_video(self, video_path: pathlib.Path,
-                        target_path: pathlib.Path,
-                        start_time=None,
-                        end_time=None):
-        # Read original video
-        original_video = mp.VideoFileClip(str(video_path))
-        fps = original_video.fps
-        total_frames = int(original_video.duration * original_video.fps)
-        start_time = 0 if start_time is None else start_time
-        end_time = original_video.duration if end_time is None else end_time
-        assert start_time <= end_time, f"Start frame{start_time} has to be smaller than end frame {end_time}"
-        assert end_time <= original_video.duration,\
-            f"End frame ({end_time}) is larger than number of frames {original_video.duration}"
-        print(*[
-            "=" * 80,
-            "Anonymizing video.",
-            f"Duration: {original_video.duration}. Total frames: {total_frames}, FPS: {fps}",
-            f"Anonymizing from: {start_time}({start_time*fps})s, to: {end_time}({end_time*fps})s"
-        ], sep="\n")
-        self.frame_idx = 0
-
-        start = original_video.subclip(0, start_time)
-        end = original_video.subclip(end_time)
-        anonymized_video = original_video.subclip(start_time, end_time)
-        anonymized_video = anonymized_video.fl_image(self.anonymize)
-        anonymized_video = mp.concatenate_videoclips([start, anonymized_video, end])
-
-
-        anonymized_video.write_videofile(
-            str(target_path),
-            fps=original_video.fps,
-            audio_codec='aac')
+    # def anonymize_video(self, video_path: pathlib.Path,
+    #                     target_path: pathlib.Path,
+    #                     start_time=None,
+    #                     end_time=None):
+    #     # Read original video
+    #     original_video = mp.VideoFileClip(str(video_path))
+    #     fps = original_video.fps
+    #     total_frames = int(original_video.duration * original_video.fps)
+    #     start_time = 0 if start_time is None else start_time
+    #     end_time = original_video.duration if end_time is None else end_time
+    #     assert start_time <= end_time, f"Start frame{start_time} has to be smaller than end frame {end_time}"
+    #     assert end_time <= original_video.duration,\
+    #         f"End frame ({end_time}) is larger than number of frames {original_video.duration}"
+    #     print(*[
+    #         "=" * 80,
+    #         "Anonymizing video.",
+    #         f"Duration: {original_video.duration}. Total frames: {total_frames}, FPS: {fps}",
+    #         f"Anonymizing from: {start_time}({start_time*fps})s, to: {end_time}({end_time*fps})s"
+    #     ], sep="\n")
+    #     self.frame_idx = 0
+    #
+    #     start = original_video.subclip(0, start_time)
+    #     end = original_video.subclip(end_time)
+    #     anonymized_video = original_video.subclip(start_time, end_time)
+    #     anonymized_video = anonymized_video.fl_image(self.anonymize)
+    #     anonymized_video = mp.concatenate_videoclips([start, anonymized_video, end])
+    #
+    #
+    #     anonymized_video.write_videofile(
+    #         str(target_path),
+    #         fps=original_video.fps,
+    #         audio_codec='aac')
