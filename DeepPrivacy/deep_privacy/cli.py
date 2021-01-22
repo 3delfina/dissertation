3d59e0c106a512e2818288d182868f39fa9e1c76
3delfina <3delfina@gmail.com>
2021-01-22 14:08:06 +0000

    Commented out print statements

--- DeepPrivacy/deep_privacy/cli.py
+++ DeepPrivacy/deep_privacy/cli.py
@@ -111,8 +111,6 @@ def get_parser() -> argparse.ArgumentParser:
 def main():
     parser = get_parser()
     args = parser.parse_args()
-    print("arguments")
-    print(args)
     anonymizer, cfg = build_anonymizer(
         args.model, opts=args.opts, config_path=args.config_path,
         return_cfg=True)
